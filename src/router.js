0d1fbf8f2695fd33b284d5add64eae0022e9225f
Cynthia Onwordi <cynthiaonwordi@Cynthias-MacBook-Pro.local>
2018-11-28 17:05:34 +0000

    removed 'handler.' as the exports is not an object (yet)

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ const handler = require("./handler");
 const router = (req, res) => {
   const url = req.url;
   if (url === "/") {
-    handler.handlerHome(req, res);
+    handler(req, res);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1> 404 not found error </h1>");
