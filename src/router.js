fa7f9b1886bd6baf0186b0f3cb06a342afc6abe1
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-28 18:01:04 +0000

    Add working files for trying to fix public server

--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
-const handler = require("./handler");
+const handler = require("./handler.js");
+
 const router = (req, res) => {
   const url = req.url;
+  console.log(url);
   if (url === "/") {
-    handler(req, res);
+    handler.handlerHome(req, res);
+  } else if (url.indexOf("public") !== -1) {
+    handler.handlerPublic(req, res, url);
+    console.log("I'm in public");
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1> 404 not found error </h1>");
