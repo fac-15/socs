424b0c6f03346582329121d3cb0fb0e3d12c39f8
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2018-11-29 11:12:04 +0000

    Fix small errors to ensure t hat server and tests work, and remove console logs

--- src/handler.js
+++ src/handler.js
@@ -1,22 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-// const request = require('request');
-// const handlers = {
-//   home: function(req, res) {
-//     const filePath = path.join(__dirname, "..", "public", "index.html");
-//     fs.readFile(filePath, (err, file) => {
-//       if (err) {
-//         console.log(err);
-//         res.writeHead(500, { "Content-Type": "text/html" });
-//         res.end("<h1>We've hit an error 500</h1>");
-//       } else {
-//         res.writeHead(200, { "Content-Type": "text/html" });
-//         res.end(file);
-//       }
-//     });
-//   },
-//   public: function(req, res) {}
-// };
+
 const handlerHome = (req, res) => {
   const filePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(filePath, (err, file) => {
--- src/router.js
+++ src/router.js
@@ -2,12 +2,10 @@ const handler = require("./handler.js");
 
 const router = (req, res) => {
   const url = req.url;
-  console.log(url);
   if (url === "/") {
     handler.handlerHome(req, res);
   } else if (url.indexOf("public") !== -1) {
     handler.handlerPublic(req, res, url);
-    console.log("I'm in public");
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<h1> 404 not found error </h1>");
