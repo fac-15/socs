9e6d3c5706db4dba0afb5f039dbbe85c1ba0d03a
Cynthia Onwordi <cynthiaonwordi@Cynthias-MacBook-Pro.local>
2018-11-30 15:00:47 +0000

    request callback added to client call

--- public/js/request.js
+++ public/js/request.js
@@ -1,32 +1,17 @@
-function fetchData(inputValue) {
+function fetchData(inputValue, callback) {
   let xhr = new XMLHttpRequest();
   //call fetch data with no params
   //print to the console
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       let coTech = JSON.parse(xhr.responseText);
+      callback(coTech);
       // return coTech;
       // console.log(coTech);
-      // console.log("******THIS IS THE XHR API WORKING******");
+      console.log("******THIS IS THE XHR API WORKING******");
     }
   };
   xhr.open("GET", `/search=${inputValue}`, true);
   xhr.send();
 }
-fetchData();
-
-document.getElementById("search").addEventListener("keyup", function(e) {
-  // e.preventDefault();
-  if (e.keyCode == 13) {
-  } else {
-    let inputValue = document.getElementById("search").value;
-
-    fetchData(inputValue);
-    //console.log(filteredObj);
-    let dataList = document.getElementById("dataList");
-
-    let dataEntry = document.createElement("OPTION");
-    dataEntry.setAttribute("value", e.name);
-    dataList.appendChild(dataEntry);
-  }
-});
+// fetchData();
